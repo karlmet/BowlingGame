6a8553e8a16317038ff54ca8908eb26a71b2a97c
Karl Métivier <Karl.Metivier@ia.ca>
2019-03-27 15:09:30 -0400

    Début du kata (setup dans VS2017)

---
+++ JS/BowlingGameJS/Scripts/BowlingGame.js
@@ -0,0 +1,15 @@
+﻿// BowlingGame.js
+(function (window) {
+    _score = 0;
+
+    function roll(pins) {
+        _score += pins;
+    };
+
+    function score() {
+        return _score;
+    };
+
+    window.roll = roll;
+    window.score = score;
+})(window);
---
+++ JS/BowlingGameJS/Specs/BowlingGameTests.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="..\Scripts/jasmine/jasmine.js" />
+/// <reference path="..\Scripts/BowlingGame.js" />
+
+describe('the bowling game',
+    function() {
+        it('correctly calculates gutter game',
+            function() {
+                // roll 20 zeros
+                for (i = 0; i < 20; i++) {
+                    roll(0);
+                }
+                var result = score();
+                expect(result).toBe(0);
+            });
+
+        it('correctly calculates all ones game', function () {
+            // roll 20 ones
+            for (i = 0; i < 20; i++) {
+                roll(1);
+            }
+            var result = score();
+            expect(result).toBe(20);
+        });
+
+    
+    });
